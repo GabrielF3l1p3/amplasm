b350e776d62f8650fbeaa02df880af7f7269e951
Gabriel Felipe da Silva Barros <barros990@hotmail.com>
2023-02-09 04:20:08 -0300

    more product

--- product/products.js
+++ product/products.js
@@ -17,5 +17,8 @@ document.querySelector('.all-products').innerHTML =
     createProduct('guiadora-haste', 'Polia Guiadora c/ haste', 'Polia de 18mm x 100mm com Haste. Utilizado para portas industriais PCF.') +
     createProduct('suspensora', 'Polia Suspensora', 'Polia de 38mm x 100mm com eixo. Utilizado para portas industriais PCF.') +
     createProduct('guiadora', 'Polia Guiadora', 'Polia de 18mm x 100mm com eixo. Utilizado para portas industriais PCF.') +
-    createProduct('selecionador-de-folhas', 'Selecionador de folhas', 'Selecionador de folhas para porta corta fogo') +
-    createProduct('calco-cunha', 'Calço cunha', 'Calço para portas industriais PCF');
+    createProduct('selecionador-de-folhas', 'Selecionador de folhas', 'Selecionador de folhas para porta corta fogo.') +
+    createProduct('calco-cunha', 'Calço cunha', 'Calço para portas industriais PCF.') + 
+    createProduct('rolo-guiador', 'Rolo Guiador', 'Rolo guiador Utilizado para portões industriais PCF.') + 
+    createProduct('base-do-rolo', 'Base do rolo guiador', 'Base do rolo guiador, Utilizado com rolo guiador.') +
+    createProduct('bucha-rosca', 'Bucha c/ rosca', 'Bucha de 45mm/40mm/30mm com rosca.');
